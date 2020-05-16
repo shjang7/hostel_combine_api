4f5f7eb66fe43e4edf9c7ccb54bd209f29604ab0
shjang7 <suhy.jang.shj@gmail.com>
2020-05-16 15:04:47 +0900

    Feature - setup MongoDB & connect MongoDB with Mongoose

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+const connectDB = require('./config/db');
+// Connect to database
+connectDB();
 // Route files
 const hostels = require('./routes/hostels');
 
@@ -18,7 +21,14 @@ app.use('/api/v1/hostels', hostels);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
