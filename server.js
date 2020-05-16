c9826c56d99facb61ee5bd2accaa4bbce730dbbd
shjang7 <suhy.jang.shj@gmail.com>
2020-05-16 14:21:17 +0900

    Chore - install morgan for dev log

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -6,8 +7,18 @@ dotenv.config({ path: './config/config.env' });
 const hostels = require('./routes/hostels');
 
 const app = express();
+
+// Dev logging morgan
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
 app.use('/api/v1/hostels', hostels);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+);
