352aa7d0f828fa9179aa473eb3059c822ae26b4d
shjang7 <suhy.jang.shj@gmail.com>
2020-05-18 15:26:03 +0900

    Feature - mongoose error handling - validation, duplicate field

--- middleware/error.js
+++ middleware/error.js
@@ -14,6 +14,18 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
+  // Mongoose duplicate key
+  if (error.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (error.name === 'ValidationError') {
+    const message = Object.values(error.errors).map(e => e.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error',
