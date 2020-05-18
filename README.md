0c95754fedfd8aed404f23055170e4687eb36980
shjang7 <suhy.jang.shj@gmail.com>
2020-05-18 15:41:52 +0900

    Feature - async handler middleware

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [javascript regex url](https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url)
 - [GeoJSON](https://mongoosejs.com/docs/geojson.html)
 - [Express JS error handling](https://expressjs.com/en/guide/error-handling.html)
+- [Express async handler](https://github.com/Abazhenov/express-async-handler)
--- controllers/hostels.js
+++ controllers/hostels.js
@@ -1,74 +1,53 @@
 const Hostel = require('../models/Hostel');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get all hostels
 // @route   GET /api/v1/hostels
 // @access  Public
-exports.getHostels = async (req, res, next) => {
-  try {
-    const hostels = await Hostel.find();
-
-    res.status(200).json({ success: true, data: hostels });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getHostels = asyncHandler(async (req, res, next) => {
+  const hostels = await Hostel.find();
+  res.status(200).json({ success: true, data: hostels });
+});
 
 // @desc    Get single hostel
 // @route   GET /api/v1/hostels/:id
 // @access  Public
-exports.getHostel = async (req, res, next) => {
-  try {
-    const hostel = await Hostel.findById(req.params.id);
-
-    res.status(200).json({ success: true, data: hostel });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getHostel = asyncHandler(async (req, res, next) => {
+  const hostel = await Hostel.findById(req.params.id);
+  res.status(200).json({ success: true, data: hostel });
+});
 
 // @desc    Create hostel
 // @route   POST /api/v1/hostels
 // @access  Private
-exports.createHostel = async (req, res, next) => {
-  try {
-    const hostel = await Hostel.create(req.body);
+exports.createHostel = asyncHandler(async (req, res, next) => {
+  const hostel = await Hostel.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: hostel,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: hostel,
+  });
+});
 
 // @desc    Update hostel
 // @route   PUT /api/v1/hostels/:id
 // @access  Private
-exports.updateHostel = async (req, res, next) => {
-  try {
-    const hostel = await Hostel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateHostel = asyncHandler(async (req, res, next) => {
+  const hostel = await Hostel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({ success: true, data: hostel });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, data: hostel });
+});
 
 // @desc    Delete hostel
 // @route   DELETE /api/v1/hostels/:id
 // @access  Private
-exports.deleteHostel = async (req, res, next) => {
-  try {
-    const hostel = await Hostel.findById(req.params.id);
+exports.deleteHostel = asyncHandler(async (req, res, next) => {
+  const hostel = await Hostel.findById(req.params.id);
 
-    await Hostel.remove();
+  await Hostel.remove();
 
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, data: {} });
+});
