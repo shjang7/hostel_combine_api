8f131fded4c9a83ae198e61058cb5e43ac5f5dbe
shjang7 <suhy.jang.shj@gmail.com>
2020-05-20 01:08:14 +0900

    Feature - updating user details and password

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,16 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resetToken', resetPassword);
 
